54f8bb20644822d4e55a9c5bdf9e3cacdce50ea8
vrajdesai78 <vrajdesai78@gmail.com>
2024-02-15 14:41:42 +0530

    Fixed UI and add more functionalities

--- huddle.js
+++ huddle.js
@@ -8,31 +8,135 @@ const client = new HuddleWebCore.HuddleClient({
   },
 });
 
-const init = async () => {
-  const roomId = document.getElementById('roomId').value;
-  const accessToken = document.getElementById('accessToken').value;
+let token = '';
+let roomId = '';
+let displayName = '';
 
-  console.log({ roomId, accessToken });
+document.querySelector('#audio').addEventListener('click', async () => {
+  const audioRef = document.querySelector('#audio');
+  if (audioRef.textContent == 'Disable Audio') {
+    await client.localPeer.disableAudio();
+    audioRef.textContent = 'Enable Audio';
+  } else {
+    await client.localPeer.enableAudio();
+    audioRef.textContent = 'Disable Audio';
+  }
+});
+
+document.querySelector('#video').addEventListener('click', async () => {
+  const videoRef = document.querySelector('#videoRef');
+
+  if (videoRef.srcObject) {
+    client.localPeer.disableVideo();
+    document.querySelector('#video').textContent = 'Enable Video';
+    return;
+  }
+
+  const stream = await client.localPeer.enableVideo();
+
+  console.log('stream', stream);
+
+  videoRef.srcObject = stream;
+  videoRef.onloadedmetadata = async () => {
+    console.warn('videoCard() | Metadata loaded...');
+    try {
+      await videoRef.play();
+      document.querySelector('#video').textContent = 'Disable Video';
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  videoRef.onerror = () => {
+    console.error('videoCard() | Error is hapenning...');
+  };
+});
+
+document.querySelector('#screen').addEventListener('click', async () => {
+  const screenRef = document.querySelector('#screenRef');
+
+  if (screenRef.srcObject) {
+    client.localPeer.stopScreenShare();
+    screenRef.srcObject = null;
+    document.querySelector('#screen').textContent = 'Share Screen';
+    return;
+  }
+
+  const stream = await client.localPeer.startScreenShare();
+
+  console.log('stream', stream);
+
+  screenRef.srcObject = stream;
+  screenRef.onloadedmetadata = async () => {
+    console.warn('videoCard() | Metadata loaded...');
+    try {
+      await screenRef.play();
+      document.querySelector('#screen').textContent = 'Stop Sharing';
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  screenRef.onerror = () => {
+    console.error('videoCard() | Error is hapenning...');
+  };
+});
+
+document.querySelector('#roomId').addEventListener('change', (e) => {
+  roomId = e.target.value;
+});
+
+document.querySelector('#accessToken').addEventListener('change', (e) => {
+  token = e.target.value;
+});
+
+document.querySelector('#displayName').addEventListener('change', (e) => {
+  displayName = e.target.value;
+});
+
+document.querySelector('#joinRoom').addEventListener('click', async () => {
   const room = await client.joinRoom({
-    roomId: roomId,
-    token: accessToken,
+    roomId,
+    token,
   });
-  console.log({ room });
-};
-
-const enableVideo = async () => {
-  await client.localPeer.enableVideo();
-  const stream = await client.localPeer.getStream({
-    label: 'video',
+  room.updateMetadata({
+    displayName: displayName,
   });
-  const video = document.getElementById('localVideo');
-  video.srcObject = stream;
-};
-
-const enableAudio = async () => {
-  await client.localPeer.enableAudio();
-};
+  document.querySelectorAll('input').forEach((input) => {
+    input.hidden = true;
+  });
+  document.querySelector('#joinRoom').hidden = true;
+});
 
 client.room.on('stream-added', ({ peerId, label }) => {
-  console.log('stream-added', peerId, label);
+  console.log(
+    'remote',
+    client.room.getRemotePeerById(peerId)?.getConsumer(label)?.track,
+    label
+  );
+  const container = document.querySelector('#remotePeers');
+  let mediaRef = document.createElement('video');
+  if (label == 'audio') {
+    mediaRef = document.createElement('audio');
+  }
+  const remoteTrack = client.room
+    .getRemotePeerById(peerId)
+    ?.getConsumer(label)?.track;
+
+  mediaRef.srcObject = new MediaStream([remoteTrack]);
+  mediaRef.id = `${peerId}-${label}`;
+  mediaRef.autoplay = true;
+  if (label == 'video') {
+    mediaRef.muted = true;
+  }
+  mediaRef.className = 'border-2 rounded-xl border-white-400 aspect-video';
+  container.appendChild(mediaRef);
+});
+
+client.room.on('stream-closed', ({ peerId, label }) => {
+  console.log('stream-closed', peerId, label);
+  const mediaRef = document.querySelector(`#${peerId}-${label}`);
+  mediaRef.srcObject.getTracks().forEach((track) => track.stop());
+  mediaRef.srcObject = null;
+  mediaRef.remove();
 });
